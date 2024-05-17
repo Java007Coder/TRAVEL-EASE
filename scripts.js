ab4ac86585ebed630ec8cd875db7353679b04030
Ravan <111245418+Java007Coder@users.noreply.github.com>
2024-05-17 22:10:43 +0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('toggle-bag').addEventListener('click', function() {
-    const shoppingBag = document.querySelector('.shopping-bag');
-    shoppingBag.classList.toggle('visible');
-    if (shoppingBag.classList.contains('visible')) {
-        shoppingBag.style.transform = 'scale(1)';
-    } else {
-        shoppingBag.style.transform = 'scale(0)';
-    }
-});
