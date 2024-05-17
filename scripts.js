9812ffdfb22bf368cd220bd036012dd8cc893a15
Ravan <111245418+Java007Coder@users.noreply.github.com>
2024-05-17 14:16:37 +0400

    Update scripts.js
    2

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Your message has been sent!');
+document.getElementById('toggle-bag').addEventListener('click', function() {
+    const shoppingBag = document.querySelector('.fas.fa-shopping-bag');
+    shoppingBag.classList.toggle('visible');
+    if (shoppingBag.classList.contains('visible')) {
+        shoppingBag.style.transform = 'scale(1)';
+    } else {
+        shoppingBag.style.transform = 'scale(0)';
+    }
 });
